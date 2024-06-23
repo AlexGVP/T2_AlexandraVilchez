3d8b445538736b57b337e1ff8ade2a927be4dace
AlexGVP <i202122156@cibertec.edu.pe>
2024-06-22 21:18:53 -0500

    pregunta4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Pregunta2Component } from './pregunta2/pregunta2.component';
+import { Pregunta3Component } from './pregunta3/pregunta3.component';
+import { Pregunta4Component } from './pregunta4/pregunta4.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "pregunta2", component: Pregunta2Component},
+    {path : "pregunta3", component: Pregunta3Component},
+    {path : "pregunta4", component: Pregunta4Component},
+];
